d7c068d74ef1244034e9399edda2af58514dce97
Denys Homilka <dhomilka@luxoft.com>
2019-12-11 18:56:33 +0200

    Added dtos

---
+++ src/main/java/com/green/testquiz/presentation/QuestionDto.java
@@ -0,0 +1,16 @@
+package com.green.testquiz.presentation;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.green.testquiz.enums.QuestionType;
+
+import lombok.Data;
+
+@Data
+public class QuestionDto {
+	String questionId;
+	String text;
+	QuestionType questionType;
+	Set<OptionDto> optionDtos = new HashSet<>();
+}
---
+++ src/main/java/com/green/testquiz/presentation/QuizDto.java
@@ -0,0 +1,19 @@
+package com.green.testquiz.presentation;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.green.testquiz.enums.QuizMode;
+
+import lombok.Data;
+
+@Data
+public class QuizDto {
+	String resultId;
+	String quizId;
+	String name;
+	String shortDescription;
+	String longDescription;
+	QuizMode quizMode;
+	Set<QuestionDto> questionDtos = new HashSet<>();
+}
