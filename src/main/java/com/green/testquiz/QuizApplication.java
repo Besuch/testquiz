820486b74b6380c30527f463bcb28cd815b2fd28
Medvedev Yakov <medvedev.teror@gmail.com>
2019-12-13 00:02:55 +0200

    QuizService added (from previous project). Init mock quiz in db if not exist.

--- src/main/java/com/green/testquiz/QuizApplication.java
+++ src/main/java/com/green/testquiz/QuizApplication.java
@@ -1,5 +1,9 @@
 package com.green.testquiz;
 
+import com.green.testquiz.datalayer.entities.Quiz;
+import com.green.testquiz.datalayer.mocks.MockQuizList;
+import com.green.testquiz.repository.QuizRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,11 +13,23 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoRepositories(basePackages = "com.green.testquiz")
 public class QuizApplication implements CommandLineRunner {
 
+    @Autowired
+    private QuizRepository quizRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(QuizApplication.class, args);
 	}
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
+        initMockQuizToMongoDb();
+    }
+
+    private void initMockQuizToMongoDb(){
+	    if (quizRepository.findAll().size()==0){
+            Quiz mockedQuiz = MockQuizList.MOCK_QUIZ_LIST.get(0);
+            quizRepository.save(mockedQuiz);
+            System.out.println("Quiz SAVED ! id=" + mockedQuiz.getQuizId().toString());
+        }
     }
 }
