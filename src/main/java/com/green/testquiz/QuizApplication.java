ce73fe89c75ed61a6bd167a8183e9038a2901ac0
OlgaKostiuk <olga.d.kostiuk@gmail.com>
2019-12-14 00:07:48 +0200

    DbInitializer refactoring

--- src/main/java/com/green/testquiz/QuizApplication.java
+++ src/main/java/com/green/testquiz/QuizApplication.java
@@ -1,35 +1,13 @@
 package com.green.testquiz;
 
-import com.green.testquiz.datalayer.entities.Quiz;
-import com.green.testquiz.datalayer.mocks.MockQuizList;
-import com.green.testquiz.repository.QuizRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = "com.green.testquiz")
-public class QuizApplication implements CommandLineRunner {
-
-    @Autowired
-    private QuizRepository quizRepository;
-
+public class QuizApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuizApplication.class, args);
 	}
-
-    @Override
-    public void run(String... args) {
-        initMockQuizToMongoDb();
-    }
-
-    private void initMockQuizToMongoDb(){
-	    if (quizRepository.findAll().size()==0){
-            Quiz mockedQuiz = MockQuizList.MOCK_QUIZ_LIST.get(0);
-            quizRepository.save(mockedQuiz);
-            System.out.println("Quiz SAVED ! id=" + mockedQuiz.getQuizId().toString());
-        }
-    }
 }
--- src/main/java/com/green/testquiz/datalayer/initDb/DbInitializer.java
+++ src/main/java/com/green/testquiz/datalayer/initDb/DbInitializer.java
@@ -1,22 +1,31 @@
 package com.green.testquiz.datalayer.initDb;
 
 import com.green.testquiz.datalayer.mocks.MockAccountList;
+import com.green.testquiz.datalayer.mocks.MockQuizList;
 import com.green.testquiz.repository.AccountRepository;
+import com.green.testquiz.repository.QuizRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
 @Component
-public class InitAccountList {
+public class DbInitializer implements CommandLineRunner {
 
     @Autowired
     private AccountRepository accountRepository;
 
-    @PostConstruct
-    private void init() {
+    @Autowired
+    private QuizRepository quizRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
         if (accountRepository.findAll().isEmpty()){
             MockAccountList.ACCOUNTS.forEach(account -> accountRepository.save(account));
+        }
+        if (quizRepository.findAll().isEmpty()){
+            MockQuizList.MOCK_QUIZ_LIST.forEach(quiz -> quizRepository.save(quiz));
         }
     }
 }
