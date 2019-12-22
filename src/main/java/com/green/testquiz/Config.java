019f1a3de0b9bb6eca76787f62a131bfeeb74098
max_sabik <Veronika_6452>
2019-12-22 23:42:27 +0200

    Removed ForwardToReactMainPageFilter.java. Added mapping for static resources.

---
+++ src/main/java/com/green/testquiz/Config.java
@@ -0,0 +1,13 @@
+package com.green.testquiz;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Config implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/public/**");
+    }
+}
